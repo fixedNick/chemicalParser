e45f2abc3e59f2dd3c639db669e380e57f5ef058
fixedNick <kkos44911@gmail.com>
2023-05-26 01:09:32 +0300

    Add project files.

---
+++ Parser.cs
@@ -0,0 +1,139 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack.CssSelectors;
+using HtmlAgilityPack.CssSelectors.NetCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chemicalParser;
+
+internal class Parser
+{
+#pragma warning disable CS8618 
+    private static Dictionary<string, string> NameKeyDict;
+#pragma warning restore CS8618 
+
+    private string baseWebsiteLink = @"https://webbook.nist.gov";
+    private string baseElementAddress = @"https://webbook.nist.gov/cgi/cbook.cgi?InChI=";
+    private string addressEnd = @"&Units=SI&cIR=on&cTZ=on";
+
+    public Parser(Dictionary<string, string> dataDict)
+    {
+        NameKeyDict = dataDict ?? throw new Exception();
+    }
+    public async Task StartParsing()
+    {
+        foreach(var kvp in NameKeyDict)
+        {
+            // @"https://webbook.nist.gov/cgi/cbook.cgi?InChI= kvp.Value &Units=SI&cIR=on&cTZ=on"
+            var linkBuilder = new StringBuilder();
+            linkBuilder.Append(baseElementAddress);
+            linkBuilder.Append(kvp.Value);
+            linkBuilder.Append(addressEnd);
+
+            // Создаем объект HtmlWeb
+            HtmlWeb web = new HtmlWeb();
+            // Загружаем HTML-страницу
+            HtmlDocument document = web.Load(linkBuilder.ToString());
+
+            var isIROnBasePage = await SearchForJCAMPFileLinkAndDownload(document, kvp.Key);
+            if (isIROnBasePage == true) continue;
+
+            var linkTags = document.QuerySelectorAll("#IR-Spec ~ ul > li > a");
+            foreach(var linkTag in linkTags)
+            {
+                var link = linkTag.GetAttributeValue("href", string.Empty);
+                if (link.Contains("ID=") && link.Contains("#IR-SPEC"))
+                {
+                    var encodedLink = new StringBuilder().Append(baseWebsiteLink).Append(link).ToString();
+                    var decodedLink = System.Net.WebUtility.HtmlDecode(encodedLink);
+                    HtmlDocument IRDoc = web.Load(decodedLink);
+                    await SearchForJCAMPFileLinkAndDownload(IRDoc, kvp.Key);
+                }
+            }
+
+        }
+    }
+
+
+    private static string DownloadsDirectory = "downloads";
+    private async Task DownloadFile(string link, string name)
+    {
+        if (Directory.Exists(DownloadsDirectory) == false) Directory.CreateDirectory(DownloadsDirectory);
+
+        var renamedFile = GetFreeFileName(DownloadsDirectory, name);
+
+        using(HttpClient httpClient = new HttpClient())
+        {
+            var decodedLink = System.Net.WebUtility.HtmlDecode(new StringBuilder().Append(baseWebsiteLink).Append(link).ToString());
+            byte[] fileData = await httpClient.GetByteArrayAsync(decodedLink);
+            await File.WriteAllBytesAsync($"{DownloadsDirectory}/{renamedFile}", fileData);
+            await Console.Out.WriteLineAsync($"Скачан файл для {name}, файл: {renamedFile}");
+        }
+    }
+
+    private string GetFreeFileName(string downloadsDirectory, string name)
+    {
+        var files = Directory.GetFiles(DownloadsDirectory);
+        int counter = 0;
+        while(true)
+        {
+            var fileName = $"{name}_{counter}.jdx";
+            if (File.Exists($"{downloadsDirectory}/{fileName}.jdx") == true)
+                counter++;
+            else return $"{fileName}.jdx";
+        }
+    }
+
+    private async Task<bool> SearchForJCAMPFileLinkAndDownload(HtmlDocument IRDoc, string name)
+    {
+        bool isLinkFound = false;
+
+        List<HtmlNode>? tableRows = IRDoc.QuerySelectorAll("table tr").ToList();
+        if (tableRows == null || tableRows.Count <= 0)
+            return await Task.FromResult(false);
+
+        foreach(var row in tableRows)
+        {
+            var tableHeader = row.QuerySelector("th");
+            if (tableHeader == null) continue;
+
+            if (tableHeader.InnerText.Trim().ToLower().Contains("resolution"))
+            {
+                var tableData = row.QuerySelector("td");
+                if (tableData == null) continue;
+
+                if (double.TryParse(tableData.InnerText.Trim(), out double val) == true)
+                {
+                    if (val < 2.0d)
+                    {
+                        await Console.Out.WriteLineAsync(ze);
+                    }
+                }
+                else
+                {
+                    await Console.Out.WriteLineAsync($"{name}. Информационный элемент Разрешение найден, но не удалось получить его значение");
+                        return await Task.FromResult(false);
+                }
+            }
+        }
+        
+        var textToSearch = "Download";
+
+        var pTags = IRDoc.QuerySelectorAll("p").ToList();
+        foreach(var p in pTags)
+        {
+            if (p.InnerText.ToLower().Trim().Contains(textToSearch.ToLower()))
+            {
+                var downloadLink = p.QuerySelector("a").GetAttributeValue("href", string.Empty);
+                DownloadFile(downloadLink, name).GetAwaiter().GetResult();
+                isLinkFound = true;
+            }
+        }
+       
+        return await Task.FromResult(isLinkFound);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System;
+using chemicalParser;
+using System.Drawing.Text;
+
+var filePath = "db.txt";
+if (File.Exists(filePath) == false)
+{
+    Console.WriteLine("Файл не найден!");
+    return;
+}
+
+Dictionary<string, string> NameAndInchiKeyDict = new Dictionary<string, string>();
+
+foreach (var line in File.ReadAllLines(filePath))
+{
+    var data = line.Split(':');
+    if (NameAndInchiKeyDict.ContainsKey(data[0])) continue;
+    NameAndInchiKeyDict.Add(data[0], data[1]);
+}
+
+var parser = new Parser(NameAndInchiKeyDict);
+await parser.StartParsing();
+
+Console.ReadKey();
